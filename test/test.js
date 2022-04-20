89e7cd7b2d3ce7b3d3d613789683d6db1439c4ac
guerzoni_andrea <guerzoni.andrea91@gmail.com>
2022-04-20 23:40:43 +0200

    linting

--- test/test.js
+++ test/test.js
@@ -1,10 +1,6 @@
-/*global describe, before, it, after, require*/
-'use strict';
-
 const stylelint = require('stylelint');
 const styleLintFormatter = require('stylelint-config-standard-scss');
 const assert = require('assert');
-const path = require('path');
 
 let lintResults = {};
 
@@ -24,14 +20,30 @@ describe('Test (test/test.js)', function () {
 			done();
 		});
 	});
+
 	it('Linting', function () {
-		// TODO assert.equal(lintResults.errored, false);
+		assert.equal(lintResults.errored, false);
 	});
-	/*
+
 	after(function () {
 		if (lintResults.errored) {
-			console.log(lintResults.output);
+			const lintArray = JSON.parse(lintResults.output);
+			for(let i = lintArray.length; i--; ){
+				const { errored = false, source = '', deprecations = [], invalidOptionWarnings = [], parseErrors = [], warnings = [] } = lintArray[i];
+				console.log(errored, source)
+				// console.log("---------- ERRORS ----------")
+				for(let k = parseErrors.length; k--; )
+					console.log(parseErrors[k]);
+				// console.log("---------- WARNINGS ----------")
+				for(let k = warnings.length; k--; )
+					console.log(warnings[k]);
+				// console.log("---------- DEPRECATIONS ----------")
+				for(let k = deprecations.length; k--; )
+					console.log(deprecations[k]);
+				// console.log("---------- INVALID OPTIONS ----------")
+				for(let k = invalidOptionWarnings.length; k--; )
+					console.log(invalidOptionWarnings[k]);
+			}
 		}
 	});
-	*/
 });
