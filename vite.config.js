3bfa9914ff3fe38dc9a11969cf378809e009cf52
Guebbit <guerzoni.andrea91@gmail.com>
2023-04-01 15:46:34 +0200

    feat: vite migration

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+    // root: "/",
+    build: {
+        outDir: path.join(__dirname, "dist"),
+        lib: {
+            entry: path.resolve(__dirname, "library.scss"),
+            name: "@guebbit/scss-library",
+            fileName: "scss-library",
+        },
+    },
+    server: {
+        port: 8080
+    },
+    resolve: {
+        alias: [
+            {
+                find: '@',
+                replacement: path.resolve(__dirname, 'src')
+            }
+        ]
+    },
+});
