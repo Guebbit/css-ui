65eb2f9d74fffa2ceee93f5c2ec783ca3b6ef4f8
Guebbit <guerzoni.andrea91@gmail.com>
2023-10-07 15:54:09 +0200

    fix

--- test/compile.test.js
+++ test/compile.test.js
@@ -2,7 +2,8 @@ const util = require("util");
 const sass = require("sass");
 const path = require("path");
 
-describe("COMPILE", () => {
+describe("COMPILE", function() {
+    this.timeout(10000);
     it("Compiles in CSS", () => {
         const sassRender = util.promisify(sass.render);
         return sassRender({
